0b484c179db43c80b9f0bab53850975b3b062b65
Raphael G <60590314+Quickndry@users.noreply.github.com>
2022-04-15 10:01:56 +0200

    Change 0.0.1
    Adapted data collection. Data is now stored in a dictionary that is then added to a list of dictionaries. Later on these will be written entry by entry into the csv output file.

--- regtotalsref.py
+++ regtotalsref.py
@@ -11,6 +11,7 @@ import re
 # are then written into target csv file.
 
 def regional_totals(infile, outfile, duplicate_checker):
+    outlist = []
     if infile.startswith("C:\\"):
         try:
             with open(infile, "r") as in_csv, open(outfile, "w", newline='') as out_csv:
@@ -25,48 +26,55 @@ def regional_totals(infile, outfile, duplicate_checker):
                     premium_profiles = html_soup.findAll("section", class_="premium_druckerei gainlayout")
                     #print("Premium Profiles: ", premium_profiles, "\nLength: ", len(premium_profiles))
                     for profile in premium_profiles:
+                        dict_storage = {}
                         profile_storage = profile.find("div", class_="row_adresse")
                         #print(profile_storage)
                         profile_string = list(profile_storage.findAll(text=True))
                         #print(profile_string, type(profile_string))
-                        name = str(profile_string[1])
-                        if name not in duplicate_checker:
-                            duplicate_checker.append(name)
-                            shop_adress = str(profile_string[2])[3:]
-                            telephone_num = str(profile_string[5])[6:]
-                            #print(profile_string[2], profile_string[3], telephone_num)
+                        dict_storage["name"] = str(profile_string[1])
+                        if dict_storage["name"] not in duplicate_checker:
+                            duplicate_checker.append(dict_storage["name"])
+                            dict_storage["adress"] = str(profile_string[2])[3:]
+                            dict_storage["telephone"] = str(profile_string[5])[6:]
                             profile_string[3] = str(profile_string[3])[3:]
-                            postcode = re.findall(r'-?\d+\.?\d*', profile_string[3])[0]
-                            town = re.sub(r'-?\d+\.?\d*', '', profile_string[3])
+                            dict_storage["postcode"] = re.findall(r'-?\d+\.?\d*', profile_string[3])[0]
+                            dict_storage["town"] = re.sub(r'-?\d+\.?\d*', '', profile_string[3])
+                            url_storage = profile.find("a", class_="hl no_deco")
+                            rough_url = url_storage.get("href")
+                            dict_storage["profile url"] = "https://www.druckereien.info/" + str(rough_url)
+                            outlist.append(dict_storage)
+                            #shop_adress = str(profile_string[2])[3:]
+                            #telephone_num = str(profile_string[5])[6:]
+                            #print(profile_string[2], profile_string[3], telephone_num)
+                            #profile_string[3] = str(profile_string[3])[3:]
+                            #postcode = re.findall(r'-?\d+\.?\d*', profile_string[3])[0]
+                            #town = re.sub(r'-?\d+\.?\d*', '', profile_string[3])
                             #print(postcode, type(postcode))
                             #print(town, type(town))
-
-                            # Get profile url
-                            url_storage = profile.find("a", class_="hl no_deco")
                             #print("profile name: ", profile_name, "type:", type(profile_name))
-                            rough_url = url_storage.get("href")
-                            premium_profile_url = "https://www.druckereien.info/" + str(rough_url)
                             #print("Premium Profile Url: ", premium_profile_url)
-                            adres_storage = profile.find("div", class_="row_adresse")
-
+                        
                     # Get standard profiles
                     standard_profiles = html_soup.findAll("section", class_="standard_druckerei gainlayout")
                     for profile in standard_profiles:
+                        dict_storage = {}
                         profile_storage = profile.find("div", class_="row_adresse")
                         #print(profile_storage)
                         profile_string = list(profile_storage.findAll(text=True))
-                        name = str(profile_string[1])
-                        if name not in duplicate_checker:
-                            duplicate_checker.append(name)
-                            shop_adress = str(profile_string[2])[3:]
-                            postcode = re.findall(r'-?\d+\.?\d*', profile_string[3])[0]
-                            town = re.sub(r'-?\d+\.?\d*', '', profile_string[3])
+                        dict_storage["name"] = str(profile_string[1])
+                        #name = str(profile_string[1])
+                        if dict_storage["name"] not in duplicate_checker:
+                            duplicate_checker.append(dict_storage["name"])
+                            dict_storage["adress"] = str(profile_string[2])[3:]
+                            dict_storage["postcode"] = re.findall(r'-?\d+\.?\d*', profile_string[3])[0]
+                            dict_storage["town"] = re.sub(r'-?\d+\.?\d*', '', profile_string[3])
                             telephone_storage = profile.find("div", class_="row_features")
                             telephone_string = list(telephone_storage.findAll(text=True)
-                            telephone_num = str(telephone_string[0])[6:])
+                            dict_storage["telephone"] = str(telephone_string[0])[6:])
                             url_storage = profile.find("a", class_="hl no_deco")
                             rough_url = url_storage.get("href")
-                            premium_profile_url = "https://www.druckereien.info/" + str(rough_url)
+                            dict_storage["profile url"] = "https://www.druckereien.info/" + str(rough_url)
+                            outlist.append(dict_storage)
                         
         except IOError:
             print("Error: cannot open file")
